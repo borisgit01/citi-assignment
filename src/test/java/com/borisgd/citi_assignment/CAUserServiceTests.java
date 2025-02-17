67f6cb6dd05d7582e8cec4e336c94794ff9c7143
Boris <borisgodunovph@gmail.com>
2025-02-17 14:04:27 -0500

    Done with tests

--- src/test/java/com/borisgd/citi_assignment/CAUserServiceTests.java
+++ src/test/java/com/borisgd/citi_assignment/CAUserServiceTests.java
@@ -30,13 +30,40 @@ class CAUserServiceTests {
 
     @BeforeEach
     void setUp() {
-        userList = Arrays.asList(
-                new CAUser(1, "James T. Kirk", "james@zzz.com"),
-                new CAUser(2, "Spock", "spock@zzz.com"),
-                new CAUser(3, "Leonard McCoy", "leonard@zzz.com"),
-                new CAUser(4, "Scotty", "scotty@zzz.com"),
-                new CAUser(5, "Nyota Uhura", "nyota@zzz.com")
-        );
+        CAUser u1 = new CAUser(1, "James T. Kirk", "james@zzz.com");
+        CAUser u2 = new CAUser(2, "Spock", "spock@zzz.com");
+        CAUser u3 = new CAUser(3, "Leonard McCoy", "leonard@zzz.com");
+        CAUser u4 = new CAUser(4, "Scotty", "scotty@zzz.com");
+        CAUser u5 = new CAUser(5, "Nyota Uhura", "nyota@zzz.com");
+        CAUser u6 = new CAUser(6, "Jean-Luc Picard", "jean@zzz.com");
+
+        u1.getFriends().add(u2);
+        u2.getFriends().add(u1);
+
+        u1.getFriends().add(u3);
+        u3.getFriends().add(u1);
+
+        u4.getFriends().add(u5);
+        u5.getFriends().add(u4);
+
+        u5.getFriends().add(u6);
+        u6.getFriends().add(u5);
+
+        userList = Arrays.asList(u1, u2, u3, u4, u5, u6);
+    }
+
+    @Test
+    void detectCommunitiesTest() {
+        when(caUserRepository.findAll()).thenReturn(userList);
+        FriendActionResponse response = caUserService.detectCommunities();
+        assertEquals("[[1, 2, 3], [4, 5, 6]]", response.getObject().toString());
+    }
+
+    @Test
+    void calculateCentralityTest() {
+        when(caUserRepository.findAll()).thenReturn(userList);
+        FriendActionResponse response = caUserService.calculateCentrality();
+        assertEquals("{1=2, 2=1, 3=1, 4=1, 5=2, 6=1}", response.getObject().toString());
     }
 
     @Test
